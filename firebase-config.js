e719105b0c959186769868ede444ad89561ca34b
Niverstan <strepoomich27@gmail.com>
2026-08-07 09:22:54 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,29 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBGFONUBgybQr0KCn_Ao_ZT9HkWVSU4jEw",
+  authDomain: "black-social-af844.firebaseapp.com",
+  projectId: "black-social-af844",
+  storageBucket: "black-social-af844.firebasestorage.app",
+  messagingSenderId: "296441938682",
+  appId: "1:296441938682:web:096a3e642bd00116f7bf43",
+  measurementId: "G-2PX2QMR8HS"
+};
+
+export const vaultConfig = {
+  apiKey: "AIzaSyCmXswh7MgI1tOcPCWl0AHBABY636gOczk",
+  authDomain: "blackvault-7a10e.firebaseapp.com",
+  projectId: "blackvault-7a10e",
+  storageBucket: "blackvault-7a10e.firebasestorage.app",
+  messagingSenderId: "1061227024519",
+  appId: "1:1061227024519:web:d7fb72171bc26f5e4ce822"
+};
+
+// Админы по email (нижний регистр)
+export const ADMIN_EMAILS = [
+  "strepoomich27@gmail.com"
+];
+
+// Пароль для входа в админку из меню (если email не в списке)
+export const ADMIN_SECRET = "blackadmin";
+
+export const COIN_PACK = { coins: 100, uah: 50 };
+export const RATES = { UAH: 1, RUB: 2.4, BYN: 0.09, USD: 0.024, KZT: 12 };
